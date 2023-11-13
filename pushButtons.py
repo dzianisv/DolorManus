c6a3ce3b7c9a917e08ea500063eb92706803edfa
Dennis Vashchuk <2119348+dzianisv@users.noreply.github.com>
2023-11-13 00:51:44 -0800

    Press enter complete

---
+++ pushButtons.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+
+# https://www.youtube.com/watch?v=Qh7lPC_Qf74&list=PLFbzd0m6AcmIRtTkAc3stBz77wRqlpMFD&index=11
+import xarm
+import enum
+
+limit=1600
+
+class Servo(enum.Enum):
+    GRIP=1
+    WRIST_ROLL=2
+    WRIST_FLEX=3
+    ELBOW=4
+    SHOULDER_LIFT=5
+    SHOULDER_PAN=6
+
+
+arm = xarm.Controller('USB', debug=True)
+arm.actionGroupRun(0)
+
+def move(states):
+    for state in states:
+        print(state)
+        arm.setPosition([[servo_id.value, angle] for servo_id, angle in state.items()], wait=True)
+
+def push_power_button():
+    cmds = [
+        {
+            Servo.ELBOW: 2100
+        },{
+            Servo.ELBOW: 2600,
+        }
+    ]
+    move(cmds)
+
+push_power_button()
